cb95e0bff8c77b3cd1842330dda89f29b63adfee
ljkkj7 <kianjenkins26@gmail.com>
2026-02-05 15:53:31 +0000

    initial model setup for signup

---
+++ backend/signup/forms.py
@@ -0,0 +1,2 @@
+from django import forms
+from backend.signup.models import ClinicParent
--- backend/signup/models.py
+++ backend/signup/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class ClinicParent(models.Model):
+    name = models.CharField(max_length=100)
+    clinic_name = models.CharField(max_length=100)
+    clinic_email = models.EmailField(unique=True)
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
